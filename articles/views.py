42e532694eb9cbf74665b870d99e37dc8477bc50
hjzheld <dusen7774@naver.com>
2023-09-26 20:36:26 +0900

    ✨ 📝 게시글 작성 기능 추가

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from .models import Article
-from .serializers import ArticleListSerializer
+from .serializers import ArticleListSerializer, ArticleCreateSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -10,5 +11,19 @@ class ArticleView(APIView):
         serializer = ArticleListSerializer(articles, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    def post(self, request):
+        serializer = ArticleCreateSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
 class ArticleDetailView(APIView):
+    pass
+    
+
+class ArticleAuthorView(APIView):
     pass
