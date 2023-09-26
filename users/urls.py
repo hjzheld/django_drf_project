1d8764ecf2084c926d40cd8fa91d2f7cdddb0a35
hjzheld <dusen7774@naver.com>
2023-09-27 01:53:38 +0900

    ✨ 📝 로그아웃 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
+    TokenBlacklistView,
 )
 from . import views
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('mock/', views.MockView.as_view(), name='mock_view'),
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
